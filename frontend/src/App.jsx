8d3101c16bcd99595f1edb8d4ce3947daf3a55a5
uday patil <uday45709@gmail.com>
2025-07-03 12:15:23 +0530

    fix error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import Hero from "./home page/Hero";
-import About from "./home page/about";
-import Service from "./home page/service";
-import Faq from "./home page/Faq";
-import ContactForm from "./home page/ContactForm";
-import Footer from "./home page/Footer";
+import Hero from "./home-page/Hero";
+import About from "./home-page/about";
+import Service from "./home-page/service";
+import Faq from "./home-page/Faq";
+import ContactForm from "./home-page/ContactForm";
+import Footer from "./home-page/Footer";
 import "./style.css";
 
 function App() {
